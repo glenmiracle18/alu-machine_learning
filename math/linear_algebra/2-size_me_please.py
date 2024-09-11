8b83aefc13f0f5e69417e933b0ea56fb848b7a50
glenmiracle18 <m.bonyu@alustudent.com>
2024-09-11 16:57:10 +0200

    hkh

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -2,22 +2,7 @@
 def matrix_shape(matrix):
     """
     A function that returns the shape of a matrix.
-
-    Parameters:
-    matrix (list): A nested list (matrix) whose shape is to be determined.
-
-    Returns:
-    list: A list representing the shape of the matrix, where each element
-          corresponds to the size of each dimension.
-
-    Example:
-    >>> matrix_shape([[1, 2], [3, 4]])
-    [2, 2]
-    >>> matrix_shape([[1], [2], [3]])
-    [3, 1]
-    >>> matrix_shape([])
-    []
-"""
+    """
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
