b0a59e4b91ac7ab532e68bbb4f863792ee97c2ad
glenmiracle18 <m.bonyu@alustudent.com>
2024-09-11 17:07:06 +0200

    did some things

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python3
+import numpy as np
 """
-A function that adds two arrays element-wise.
+    A function that transposes a matrix
 """
-
-
-def add_arrays(arr1, arr2):
+def matrix_transpose(matrix):
     """
-    Return the sum of two arrays.
+    Return the transpose of a 2D matrix.
     """
-    # check if the array is not of the same shape
-    if len(arr1) != len(arr2):
-        return None
-    # loop through the array and add the elements
-    new_array = []
-    for i in range(len(arr1)):
-        sum = arr1[i] + arr2[i]
-        new_array.append(sum)
-    return new_array
+    return np.array(matrix).T.tolist()
