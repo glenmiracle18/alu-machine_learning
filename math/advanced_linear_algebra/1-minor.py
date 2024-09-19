c6da7125e3c0b119249b9a1842dddc9df660b2ce
glenmiracle18 <m.bonyu@alustudent.com>
2024-09-19 18:47:43 +0200

    this should work please

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -12,7 +12,7 @@ def minor(matrix):
             isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
 
-    if not matrix or len(matrix) != len(matrix[0]):
+    if not matrix or not all(len(row) == len(matrix) for row in matrix):
         raise ValueError("matrix must be a non-empty square matrix")
 
     n = len(matrix)
