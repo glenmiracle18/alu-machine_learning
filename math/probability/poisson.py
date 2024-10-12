f94ff4941f860f898c1eca43fb23dcbae0bf7d41
glenmiracle18 <m.bonyu@alustudent.com>
2024-10-12 21:42:32 +0200

    done

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -12,15 +12,6 @@ class Poisson:
     def __init__(self, data=None, lambtha=1.):
         """
         Initialize the Poisson distribution
-
-        Args:
-            data (list, optional): List of data to estimate the distribution. Defaults to None.
-            lambtha (float, optional): Expected number of occurrences in a given time frame. Defaults to 1.
-
-        Raises:
-            ValueError: If lambtha is not positive when data is None
-            TypeError: If data is provided but is not a list
-            ValueError: If data is provided but contains fewer than two values
         """
         if data is None:
             if lambtha <= 0:
@@ -35,13 +26,7 @@ class Poisson:
 
     def pmf(self, k):
         """
-        Calculates the value of the PMF for a given number of "successes"
-
-        Args:
-            k (int): The number of "successes"
-
-        Returns:
-            float: The PMF value for k
+        Calculates the value of the PMF for a number
         """
         k = int(k)
         if k < 0:
