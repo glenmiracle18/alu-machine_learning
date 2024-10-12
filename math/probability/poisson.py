c8362257ec59e1e49f4b1f8355534680ee561d8e
glenmiracle18 <m.bonyu@alustudent.com>
2024-10-12 21:50:34 +0200

    done

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,58 +1,94 @@
 #!/usr/bin/env python3
-"""
-Create a class Poisson that represents a poisson distribution:
-"""
+""" defines Poisson class that represents Poisson distribution """
 
 
 class Poisson:
     """
-    The Poisson class
+    class that represents Poisson distribution
+
+    class constructor:
+        def __init__(self, data=None, lambtha=1.)
+
+    instance attributes:
+        lambtha [float]: the expected number of occurances in a given time
+
+    instance methods:
+        def pmf(self, k): calculates PMF for given number of successes
+        def cdf(self, k): calculates CDF for given number of successes
     """
 
     def __init__(self, data=None, lambtha=1.):
         """
-        Initialize the Poisson distribution
+        class constructor
+
+        parameters:
+            data [list]: data to be used to estimate the distibution
+            lambtha [float]: the expected number of occurances on a given time
+
+        Sets the instance attribute lambtha as a float
+        If data is not given:
+            Use the given lambtha or
+            raise ValueError if lambtha is not positive value
+        If data is given:
+            Calculate the lambtha of data
+            Raise TypeError if data is not a list
+            Raise ValueError if data does not contain at least two data points
         """
         if data is None:
-            if lambtha <= 0:
+            if lambtha < 1:
                 raise ValueError("lambtha must be a positive value")
-            self.lambtha = float(lambtha)
+            else:
+                self.lambtha = float(lambtha)
         else:
-            if not isinstance(data, list):
+            if type(data) is not list:
                 raise TypeError("data must be a list")
-            if len(data) < 2:
+            elif len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = float(sum(data) / len(data))
+            else:
+                lambtha = float(sum(data) / len(data))
+                self.lambtha = lambtha
 
     def pmf(self, k):
         """
-        Calculates the value of the PMF for a number
+        calculates the value of the PMF for a given number of successes
+
+        parameters:
+            k [int]: number of successes
+                If k is not an int, convert it to int
+                If k is out of range, return 0
+
+        return:
+            the PMF value for k
         """
-        k = int(k)
+        if type(k) is not int:
+            k = int(k)
         if k < 0:
             return 0
-        return (self.exp(-self.lambtha) * (self.lambtha ** k)) / self.factorial(k)
+        e = 2.7182818285
+        lambtha = self.lambtha
+        factorial = 1
+        for i in range(k):
+            factorial *= (i + 1)
+        pmf = ((lambtha ** k) * (e ** -lambtha)) / factorial
+        return pmf
 
-    def exp(self, x):
+    def cdf(self, k):
         """
-        Calculates the exponential of x
-        """
-        result = 1.0
-        term = 1.0
-        n = 1
-        while term > 1e-15:  # A small threshold for convergence
-            term *= x / n
-            result += term
-            n += 1
-        return result
+        calculates the value of the CDF for a given number of successes
 
-    def factorial(self, n):
+        parameters:
+            k [int]: number of successes
+                If k is not an int, convert it to int
+                If k is out of range, return 0
+
+        return:
+            the CDF value for k
         """
-        Calculates the factorial of n
-        """
-        if n == 0 or n == 1:
-            return 1
-        result = 1
-        for i in range(2, n + 1):
-            result *= i
-        return result
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
+        cdf = 0
+        for i in range(k + 1):
+            cdf += self.pmf(i)
+        return cdf
